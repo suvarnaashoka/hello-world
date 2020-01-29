97213d284458938932a0339a4b689cae9aa3f1c7
suvarnaashoka <60419761+suvarnaashoka@users.noreply.github.com>
2020-01-29 12:52:07 +0530

    Add files via upload

---
+++ AuditableEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Poc.Entities
+{
+    public abstract class AuditableEntity  
+    {
+        [ScaffoldColumn(false)]
+        [IgnoreDataMember]
+        public DateTime SystemCreatedDateTime { get; set; }
+
+        [ScaffoldColumn(false)]
+        [IgnoreDataMember]
+        public DateTime UserCreatedDateTime { get; set; }   
+
+        [ScaffoldColumn(false)]
+        [IgnoreDataMember]
+        public int CreatedBy { get; set; }
+
+        [ScaffoldColumn(false)]
+        [IgnoreDataMember]
+        public DateTime? UpdatedDateTime { get; set; }  
+        
+        [ScaffoldColumn(false)]
+        [IgnoreDataMember]
+        public int? UpdatedBy { get; set; }
+
+        public int Id { get; set; }
+
+        [IgnoreDataMember]
+        public bool DeleteFlag { get; set; }
+    }
+}
